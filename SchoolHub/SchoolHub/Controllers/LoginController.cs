fa22d2f68782852ee043112b22f34d72ffc80027
John Kasper <kaspej12@uwosh.edu>
2018-11-06 17:06:38 -0600

    Added CheckUserLogin and LoginUser functions to LoginController.

--- SchoolHub/SchoolHub/Controllers/LoginController.cs
+++ SchoolHub/SchoolHub/Controllers/LoginController.cs
@@ -3,14 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SchoolHub.Infrastructure.Containers;
+using SchoolHub.Models;
 
 namespace SchoolHub.Controllers
 {
     public class LoginController : Controller
     {
+        SchoolhubDb db = new SchoolhubDb();
         public ActionResult Index()
         {
             return View();
+        }
+
+        public string CheckUserLogin(string username, string password)
+        {
+            User user = this.db.GetUserByUsernamePassword(username, password);
+            if(user == null)
+            {
+                return "Incorrect username or password.";
+            }
+            this.LoginUser(user);
+            return "";
+        }
+
+        public ActionResult LoginUser(User user)
+        {
+            if(user.RoleId == 1)
+            {
+                return RedirectToAction("Index", "TeacherHome", new { user = user });
+            }
+            return RedirectToAction("Index", "StudentHome");
         }
     }
 }
