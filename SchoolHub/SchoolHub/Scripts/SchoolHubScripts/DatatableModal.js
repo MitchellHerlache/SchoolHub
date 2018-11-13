35bcbb86f60db5d9dcf0e6997e98dce155049da9
Josiah Poss <possj96@uwosh.edu>
2018-11-13 07:37:41 -0600

    Updates to the teacher home

---
+++ SchoolHub/SchoolHub/Scripts/SchoolHubScripts/DatatableModal.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(function () {
+
+    var table = $('#example').DataTable({
+        responsive: {
+            details: {
+                display: $.fn.dataTable.Responsive.display.modal({
+                    header: function (row) {
+                        var data = row.data();
+                        return 'Details for class # ' + data[0];
+                    }
+                }),
+                renderer: $.fn.dataTable.Responsive.renderer.tableAll({
+                    tableClass: 'table'
+
+                })
+            }
+        },
+        colReorder: true,
+        // "order": [[3, "desc"]],
+    });
+});
