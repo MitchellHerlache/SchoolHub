f8715a7a0412ed996f8fb53dfe260da2a0f81b47
Mitchell Herlache <herlam13@uwosh.edu>
2018-12-01 18:53:49 -0600

    Student home table now resizes like teacher home

--- SchoolHub/SchoolHub/Scripts/SchoolHubScripts/StudentHomeTable.js
+++ SchoolHub/SchoolHub/Scripts/SchoolHubScripts/StudentHomeTable.js
@@ -13,6 +13,21 @@ $(document).ready(function () {
             null,
             null,
             { "orderDataType": "dom-checkbox" }
-        ]
+        ],
+        responsive: {
+            details: {
+                display: $.fn.dataTable.Responsive.display.modal({
+                    header: function (row) {
+                        var data = row.data();
+                        return 'Details for class # ' + data[0];
+                    }
+                }),
+                renderer: $.fn.dataTable.Responsive.renderer.tableAll({
+                    tableClass: 'table'
+
+                })
+            }
+        },
+        colReorder: true,
     });
 });
